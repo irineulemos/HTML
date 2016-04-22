ae26b62ac22130533eacda8ce6ef1318563cf059
Irineu <irineuljr@gmail.com>
2016-04-22 19:09:35 -0300

    Atividade 04
    Correção da atividade 04 que não estava mostrando o mapa corretamente.

--- javascripts/meusScripts.js
+++ javascripts/meusScripts.js
@@ -21,13 +21,6 @@ function initMap() {
     
     var msgPopupUVV = '<span>você está na uvv</span>';
     var msgPopupShopping = '<span>você está no shopping</span>';
-   
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18,
-        id: 'your.mapbox.project.id',
-        accessToken: 'your.mapbox.public.access.token'
-    }).addTo(mymap);
 
 /*demarca a uvv*/
 var pUvv = L.polygon([
